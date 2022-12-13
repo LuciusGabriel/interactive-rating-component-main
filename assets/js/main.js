032621d47612e2e6746f05e8da2547d588f2a639
LuciusGabriel <lucius_gabriel@hotmail.com>
2022-12-12 23:05:08 -0300

    Click inativo

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,5 +2,7 @@ const buttonSubmit = document.querySelector('.btn')
 const wrapper = document.querySelectorAll('.wrapper')
 
 buttonSubmit.addEventListener('click', ()=>{
-    
+  wrapper.forEach((item)=>{
+    item.classList.toggle('inativo')
+  })  
 })
