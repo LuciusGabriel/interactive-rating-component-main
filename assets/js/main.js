1fe5b22891b891e6175ed58409a0a716ef99ae0a
LuciusGabriel <lucius_gabriel@hotmail.com>
2022-12-14 15:02:43 -0300

    finaliza primeira versão JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,18 @@
 const btnSubmit = document.querySelector('.btn')
 const wrapper = document.querySelectorAll('.wrapper')
 const btnRating = document.querySelectorAll('.btn-rating')
+const rating = document.querySelector('.rating')
 let ratingValue = 0
 
+
+
 btnSubmit.addEventListener('click', ()=>{
-  wrapper.forEach((item)=>{
-    item.classList.toggle('inativo')
-  })  
+  if(ratingValue!=0){
+    wrapper.forEach((item)=>{
+      item.classList.toggle('inativo')
+    })
+    rating.insertAdjacentHTML('afterbegin', `<p>You selected ${ratingValue} out of 5</p>`)
+  }
 })
 
 btnRating.forEach((item)=>{
