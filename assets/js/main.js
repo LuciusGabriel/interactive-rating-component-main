dc46aceb25f81a5f7eeb0242897c8e4667c937ce
LuciusGabriel <lucius_gabriel@hotmail.com>
2022-12-13 23:13:24 -0300

    Estabelecendo as Notas

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,17 @@
-const buttonSubmit = document.querySelector('.btn')
+const btnSubmit = document.querySelector('.btn')
 const wrapper = document.querySelectorAll('.wrapper')
+const btnRating = document.querySelectorAll('.btn-rating')
+let ratingValue = 0
 
-buttonSubmit.addEventListener('click', ()=>{
+btnSubmit.addEventListener('click', ()=>{
   wrapper.forEach((item)=>{
     item.classList.toggle('inativo')
   })  
+})
+
+btnRating.forEach((item)=>{
+  item.addEventListener('click', ()=>{
+    ratingValue = item.value
+    console.log(ratingValue)
+  })
 })
