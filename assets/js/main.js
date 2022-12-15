0c50a40c06d2eb0361285961fae061cd042fc57f
LuciusGabriel <lucius_gabriel@hotmail.com>
2022-12-15 11:06:27 -0300

    Afinando

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,13 +11,18 @@ btnSubmit.addEventListener('click', ()=>{
     wrapper.forEach((item)=>{
       item.classList.toggle('inativo')
     })
-    rating.insertAdjacentHTML('afterbegin', `<p>You selected ${ratingValue} out of 5</p>`)
+    rating.insertAdjacentHTML('afterbegin', `<figure class="thank-img">
+    <img src="./images/illustration-thank-you.svg" alt="">
+  </figure><p class="rating-select">You selected ${ratingValue} out of 5</p>`)
   }
 })
 
 btnRating.forEach((item)=>{
   item.addEventListener('click', ()=>{
+    item.classList.remove('rating-ativo')
     ratingValue = item.value
+    item.classList.add('rating-ativo')
     console.log(ratingValue)
   })
+  
 })
